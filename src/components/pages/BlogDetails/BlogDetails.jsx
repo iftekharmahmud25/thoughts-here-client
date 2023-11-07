ffe4b26a5776e98071bce74716560e8775576432
iftekahrmahmud98@gmail.com <youremail@example.com>
2023-11-07 19:37:26 +0600

    added comment

--- src/components/pages/BlogDetails/BlogDetails.jsx
+++ src/components/pages/BlogDetails/BlogDetails.jsx
@@ -1,11 +1,57 @@
-import { useContext } from "react";
-import { useLoaderData } from "react-router-dom";
+import { useContext, useState } from "react";
+import { Navigate, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../../provider/AuthProvider";
+import { toast } from "react-toastify";
 
 const BlogDetails = () => {
     const blogDetailsInfo = useLoaderData();
     const { user } = useContext(AuthContext)
+    const [commentText, setCommentText] = useState('');
+    const [comments, setComments] = useState(blogDetailsInfo.comments || []);
+    console.log("User:", user);
+    if (!user) {
+      return <Navigate to="/login" />;
+    }
     const timestamp = new Date(blogDetailsInfo.timestamp).toLocaleString();
+
+    const addComment = (e) => {
+        e.preventDefault();
+    
+        const newComment = {
+          text: commentText,
+          name: user.displayName,
+          photoUrl: user.photoURL,
+        };
+    
+        fetch(`http://localhost:5000/blogs/${blogDetailsInfo._id}`, {
+          method: 'PUT',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(newComment),
+        })
+          .then((response) => {
+            if (response.ok) {
+              setComments([...comments, newComment]);
+              toast.success('Successfully added comment');
+              console.log('Successfully added comment');
+            } else {
+              toast.error('Error adding comment');
+              console.error('Error adding comment');
+            }
+          })
+          .catch((error) => {
+            console.error('Network error:', error);
+          });
+    
+        setCommentText('');
+      };
+
+
+
+
+
+
 
     return (
         <div className="w-[80%] mx-auto">
@@ -26,23 +72,40 @@ const BlogDetails = () => {
               
             
 
-            <div className="mb-20 mt-6">
-                <h2>Comments</h2>
-                <div className="">
-                    <textarea
-                       className="w-[100%] outline p-3"
-                        
-                    ></textarea>
-                   <div className=""> <button className="btn btn-sm bg-black text-white hover:bg-black hover:text-white">Add Comment</button></div>
+            <div className="mt-8 mb-20 border p-5">
+        <h2 className="text-xl font-semibold mb-4">Comments</h2>
+        <form className="mb-4" onSubmit={addComment} >
+          <textarea
+            rows="2"
+            name="comments"
+            placeholder="Add a comment..."
+            className="w-full px-3 py-2 border rounded-md resize-none focus:outline-[#197685] focus:border-[#197685]"
+            style={{ border: "1px solid #ccc" }}
+            value={commentText}
+            onChange={(e) => setCommentText(e.target.value)}
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-[#197685]  btn btn-sm hover:bg-white hover:text-[#197685] hover:border-[#197685]  hover:shadow-xl text-white h-10 px-4 rounded"
+          >
+            post
+          </button>
+        </form> <div className="rounded-lg">
+          {comments?.map((comment, index) => (
+            <div key={index} className="mb-3 p-3 bg-gray-50 rounded-xl">
+              <div className="chat chat-start">
+                <div className="chat-image avatar">
+                  <div className="w-10 rounded-full">
+                    <img src={comment.photoUrl} alt={comment.name} />
+                  </div>
                 </div>
-                <div>
-                    {/* {comments.map((comment) => (
-                        <div key={comment._id}>
-                            <p>{comment.text}</p>
-                        </div>
-                    ))} */}
-                </div>
+                <div className="chat-header">{comment.name}</div>
+                <div className="chat-bubble">{comment.text}</div>
+              </div>
             </div>
+          ))}
+        </div>
+      </div>
         </div>
     );
 };
