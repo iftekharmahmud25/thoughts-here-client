d449ced2e71965ea8bf475ffdb43d74a7506050a
iftekahrmahmud98@gmail.com <youremail@example.com>
2023-11-07 14:11:57 +0600

    designed details page

--- src/components/pages/BlogDetails/BlogDetails.jsx
+++ src/components/pages/BlogDetails/BlogDetails.jsx
@@ -4,8 +4,12 @@ const BlogDetails = () => {
     const blogDetailsInfo = useLoaderData();
     console.log(blogDetailsInfo)
     return (
-        <div>
-            
+        <div className="w-[80%] mx-auto">
+            <p className="mb-8 whitespace-normal mt-16 md:mt-20 font-bold text-center text-xl md:text-4xl">{blogDetailsInfo?.title}</p>
+            <img className="w-[90%] md:w-[70%] h-[130px] md:h-[300px] mx-auto mb-12 rounded-sm shadow-xl" src={blogDetailsInfo?.imageUrl} alt="" />
+            <p className="capitalize my-4 font-semibold text-lg md:text-2xl">{blogDetailsInfo?.shortDescription}</p>
+            <p className="indent-12 text-base text-justify mb-6 text-gray-700">{blogDetailsInfo?.longDescription}</p>
+            <div className="divider mb-12"></div>
         </div>
     );
 };
